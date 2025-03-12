eea6fd2e7921cf57fd16e3920c744c667ea46d91
DevMytho <devmytho@gmail.com>
2025-03-12 19:46:44 +0530

    req error in contact/route

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   try {
     const { name, email, message } = await req.json();
 
