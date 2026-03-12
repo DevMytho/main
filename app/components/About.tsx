6991c0b34654c517f42df038e11797a463d8208d
Devvv <118110516+DevMytho@users.noreply.github.com>
2026-03-12 20:20:23 +0530

    Update resume link and text in About component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -68,12 +68,19 @@ export default function About() {
               {" "}
               Drop me an email
             </a>{" "}
-            or{" "}
+            or {" "}
             <a
-              href="#contact"
+              href="/assets/resume.pdf" target="_blank"
               className="text-blue-400 underline italic hover:text-blue-500 transition-colors duration-200"
             >
-              request my resume!
+              View my resume
+            </a>{" "}
+            or {" "}
+            <a
+              href="/assets/resume.pdf" download="Debasish_Mishra_Resume.pdf"
+              className="text-blue-400 underline italic hover:text-blue-500 transition-colors duration-200"
+            >
+              Download my resume!
             </a>
           </p>
         </motion.div>
@@ -81,3 +88,4 @@ export default function About() {
     </motion.div>
   );
 }
+
